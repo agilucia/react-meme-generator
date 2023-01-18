aceada528478bf000df72c4c045bd5c0390d2c34
Agnes Pilar <agnes.pilar@yahoo.de>
2023-01-18 17:49:39 +0100

    Add first working version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { saveAs } from 'file-saver';
 
-function App() {
+export default function App() {
+  const [top, setTop] = useState('');
+  const [bottom, setBottom] = useState('');
+  const [meme, setMeme] = useState('spiderman');
+  const memeUrls = (memes, tops, bottoms) => {
+    if (!tops && !bottoms) {
+      return `https://api.memegen.link/images/${memes}.png`;
+    } else if (!bottoms) {
+      return `https://api.memegen.link/images/${memes}/${tops}.png`;
+    } else if (!tops) {
+      return `https://api.memegen.link/images/${memes}/_/${bottoms}.png`;
+    } else {
+      return `https://api.memegen.link/images/${memes}/${tops}/${bottoms}.png`;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>React Meme Generator</h1>
+      <br />
+      <div>
+        <img
+          src={memeUrls(meme, top, bottom)}
+          alt="meme"
+          data-test-id="meme-image"
+        />
+      </div>
+      <br />
+      <label>
+        Meme template
+        <input
+          value={meme}
+          onChange={(event) => {
+            setMeme(event.currentTarget.value);
+          }}
+        />
+      </label>
+      <br />
+      <label>
+        Top text
+        <input
+          value={top}
+          onChange={(event) => {
+            setTop(event.currentTarget.value);
+          }}
+        />
+      </label>
+      <br />
+      <label>
+        Bottom text
+        <input
+          value={bottom}
+          onChange={(event) => {
+            setBottom(event.currentTarget.value);
+          }}
+        />
+      </label>
+      <br />
+      <div>
+        <button
+          onClick={() => {
+            saveAs(memeUrls(meme, top, bottom), 'meme.png');
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Download
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
